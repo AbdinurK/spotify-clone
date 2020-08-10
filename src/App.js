7e1313ed1dd8800939abcc94d22acd54461ea409
Kuatbek Abdinur <kuatbekabdinur@gmail.com>
2020-08-10 18:14:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Login from "./components/Login/Login";
 import './App.css';
+import {getTokenFromUrl} from "./services/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./context/DataLayer"
+import Player from "./components/Player/Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+    const [{ user, token }, dispatch] = useDataLayerValue();
+
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = "";
+        const _token = hash.access_token;
+        if (_token) {
+            dispatch({
+                type: "SET_TOKEN",
+                token: _token
+            })
+            spotify.setAccessToken(_token)
+            spotify.getMe().then(user => {
+                dispatch({
+                    type: "SET_USER",
+                    user: user
+                })
+            }).catch(e => console.log(e))
+
+            spotify.getUserPlaylists().then((playlists) => {
+                dispatch({
+                    type: "SET_PLAYLIST",
+                    playlists: playlists
+                })
+            })
+        }
+
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+            token ? (
+                <Player spotify={spotify}/>
+            ) : (
+                <Login/>
+            )
+        }
     </div>
   );
 }
---
+++ src/context/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+    user: null,
+    token: 'BQCY2vuadYaOGzSKMd4M1GeKBIFb68_lvQdM_138oSr7h4EFs4XK23-vJ-egPUxypOgTr5pi0HJOb6jubrxqDg9nZdcK78DN6p1JyQGr9dFM8CvCQvvSlEkGK1HLh8ntLUDXgoKFEFSX3yGwdCnHoPmDKpdAids2DOF70clJ2W8J98Mb',
+    playlists: [],
+    playing: false,
+    item: null
+};
+
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user
+            };
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token
+            };
+        case "SET_PLAYLIST":
+            return {
+                ...state,
+                playlists: action.playlists
+            }
+        default:
+            return state
+    }
+};
+
+export default reducer;
